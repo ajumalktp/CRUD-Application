b0550866ed2cbf25b5c804db95e3d235be4d0f76
ajumal <ajumalktp555@gmail.com>
2023-05-16 11:12:26 +0530

    files added and modified

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,17 @@
 const express =require('express')
 const router = express.Router();
-const {getUserHome, getLogin, getSignup, userLogout,signUp, userLogin} =require('../controller/userController')
+const {getUserHome, getLogin, getSignup, userLogout,signUp, userLogin,getUserEdit,userEdit} =require('../controller/userController')
 const userModel =require('../models/userModel')
 
 router.get('/',getUserHome)
 router.get('/login',getLogin)
 router.get('/signUp',getSignup)
 router.get('/logOut',userLogout)
+router.get('/edit/:id',getUserEdit)
 
 router.post('/login',userLogin)
 router.post('/signUp',signUp)
+router.post('/edit-user',userEdit)
 
 
 module.exports = router;
